0e28f93c0755f92b7994b0da9d68684e50b6c0e4
Joaggi <joaggi@gmail.com>
2014-01-06 21:59:00 -0500

    Commit
    Joseph

--- Code/Prueba.py
+++ Code/Prueba.py
@@ -13,7 +13,7 @@ from ComunidadAgentes import ComunidadAgentes
 Ingenieros = ComunidadAgentes("estudiantes")
 #Metodo pyro
 #Se crea de la manera simple con serveSimple, sin tener en cuenta el host.
-Pyro4.Daemon.serveSimple({e
+Pyro4.Daemon.serveSimple({
     Ingenieros : "agentes.distribuidos.comunidad." + str(Ingenieros.getNombre())
 },host="192.168.1.14")
 
--- Code/Servidor.py
+++ Code/Servidor.py
@@ -9,5 +9,5 @@ import Pyro4
 # we're using custom classes, so need to use pickle
 Pyro4.config.SERIALIZERS_ACCEPTED.add('pickle')
 
-Pyro4.naming.startNSloop(host = "0.0.0.0")
-#Pyro4.naming.startNSloop(host = "192.168.1.10")
+#Pyro4.naming.startNSloop(host = "0.0.0.0")
+Pyro4.naming.startNSloop(host = "192.168.1.10")
